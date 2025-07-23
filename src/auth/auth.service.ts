af407fd0f921d3e72efd02e3d2257ca3fbaebc4d
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-07-23 07:22:07 +0100

    init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    signin() {
+        return {message:'I am signed in'}
+    }
+
+    signup(){
+        return {message:'I am signed up'}
+    }
+}
