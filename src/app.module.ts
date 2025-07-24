fdca81080817349dc7cbf8280325261c9038db4f
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-07-24 14:32:26 +0100

    changed the output of prisma to be the default prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }),AuthModule, UserModule, BookmarkModule],
+  }),AuthModule, UserModule, BookmarkModule, PrismaModule],
+  providers: [],
 
 })
 export class AppModule {}
