e68713af3082461a46d4dd70a96201feac950355
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-07-25 17:58:19 +0100

    integrated jwt and added pactum for testing

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { getUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@getUser() user: User){
+        return user;
+    }
+
+    @Patch()
+    editUser() {
+        return
+    }
+}
