ee412796da7a07c263a47fa6797b0e9b6ab28fcb
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-07-25 06:32:44 +0100

    added signup and login functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,61 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService){}
-    signin() {
-        return {message:'I am signed in'}
-    }
 
-    signup(){
-        return {message:'I am signed up'}
+    
+    async signup(dto: AuthDto){
+        try {
+    
+            const hash = await argon.hash(dto.password);
+    
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                },
+                
+            })
+            
+            const { hash: _, ...userWithoutHash } = user;
+            
+            return userWithoutHash;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if (error.code === 'P2002'){
+                    throw new ForbiddenException("Credentials taken")
+                }
+            }
+
+            throw error
+        }        
+    }
+    
+    async signin(dto: AuthDto) {
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+
+        if(!user){
+            throw new ForbiddenException('Credentials incorrect')
+        }
+
+        const pwMatch = await argon.verify(user.hash, dto.password)
+
+        if(!pwMatch) throw new ForbiddenException("Credentials incorrect")
+
+        const { hash: _, ...userWithoutHash } = user;
+
+        return userWithoutHash
+
     }
 }
